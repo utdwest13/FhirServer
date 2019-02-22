1753329bf2b37afcdaea909f09c7caf4e5463439
utdwest13 <utdwest13@gmail.com>
2019-02-22 16:40:14 +0900

    init

---
+++ src/main/java/com/smc/main/SampleFhirServerApplication.java
@@ -0,0 +1,14 @@
+package com.smc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleFhirServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleFhirServerApplication.class, args);
+	}
+
+}
+
